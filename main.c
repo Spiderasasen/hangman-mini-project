870a8484e6f4d55e948402b1f3fcbeb508e9c79c
spiderasasen <spiderasasen@gmail.com>
2026-02-24 09:46:53 -0500

    making my version of an array list

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+//defining vars
+#define MAX_LINES 100
+#define MAX_LEN 100
+
+//making a simple way to return strings
+typedef struct {
+    char lines[MAX_LINES][MAX_LEN];
+    int count;
+} StringArray;
+
 int main() {
     printf("Hello world!");
     return 0;
